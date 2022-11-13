e647bea50d614a752b39a2da792f96583547f40e
utka-nos <dimikmusik@yandex.ru>
2022-11-13 22:14:43 +0300

    fix

---
+++ coordinator-service/src/main/java/com/example/CommonController.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.apache.catalina.connector.Request;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+    @GetMapping("/")
+    public String getRoot() {
+        return "ok: /";
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "ok: /users";
+    }
+
+    @GetMapping("/*")
+    public String getAnother(Request request) {
+        return request.getRequestURI();
+    }
+
+}
